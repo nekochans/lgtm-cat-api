d951ea17818e398cc1caad9c34636b0c821c8cc6
kobayashi-m42 <m42kobayashi@gmail.com>
2024-09-28 13:57:23 +0900

    ruff formatを実行

--- src/main.py
+++ src/main.py
@@ -5,32 +5,53 @@ import uvicorn
 
 app = FastAPI(title="LGTM Cat API")
 
+
 class LgtmImageRandomListResponse(BaseModel):
     id: str
     url: str
+
 
 class LgtmImageRecentlyCreatedListResponse(BaseModel):
     id: str
     url: str
 
+
 class LgtmImageCreateRequest(BaseModel):
     image: str
     imageExtension: str
 
+
 class LgtmImageCreateResponse(BaseModel):
     imageUrl: str
 
+
 @app.get("/lgtm-images", response_model=List[LgtmImageRandomListResponse])
 async def extract_random_lgtm_images():
-    return [LgtmImageRandomListResponse(id="1", url="https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp")]
+    return [
+        LgtmImageRandomListResponse(
+            id="1",
+            url="https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp",
+        )
+    ]
+
 
 @app.post("/lgtm-images", response_model=LgtmImageCreateResponse)
 async def create_lgtm_image(lgtm_image_create: LgtmImageCreateRequest):
-    return LgtmImageCreateResponse(imageUrl="https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp")
+    return LgtmImageCreateResponse(
+        imageUrl="https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp"
+    )
 
-@app.get("/lgtm-images/recently-created", response_model=LgtmImageRecentlyCreatedListResponse)
+
+@app.get(
+    "/lgtm-images/recently-created", response_model=LgtmImageRecentlyCreatedListResponse
+)
 async def extract_recently_created_lgtm_images():
-    return [LgtmImageRecentlyCreatedListResponse(id="1", url="https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp")]
+    return [
+        LgtmImageRecentlyCreatedListResponse(
+            id="1",
+            url="https://lgtm-images.lgtmeow.com/2021/03/16/23/5947f291-a46e-453c-a230-0d756d7174cb.webp",
+        )
+    ]
 
 
 def start():
